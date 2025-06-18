9eaf73ac5670ad50bf5f71caee410a283a3786b2
Aslam Sunkesula <100505205+AslamSunkesula@users.noreply.github.com>
2025-06-18 19:00:41 +0530

    Update dev.tf
    added comments

--- SQS/dev.tf
+++ SQS/dev.tf
@@ -1,5 +1,5 @@
 module "standard_queue" {
-  source      = "../modules/sqs"
+  source      = "../modules/sqs" //local path to the SQS module 
   name        = "my-standard-queue"
   kms_master_key_id = data.aws_kms_alias.sqs_kms_key.target_key_id
   fifo_queue  = false
@@ -11,7 +11,7 @@ module "standard_queue" {
 }
 
 module "fifo_queue_with_dlq" {
-  source      = "../modules/sqs"
+  source      = "../modules/sqs" //local path to the SQS module 
   name        = "my-fifo-queue"
   kms_master_key_id = data.aws_kms_alias.sqs_kms_key.target_key_id
   fifo_queue  = true
